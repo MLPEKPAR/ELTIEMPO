7032e5ab55e2658615d5db293d42f18a1cef070e
MLPEKPAR <rose.marialuisa@gmail.com>
2025-01-25 19:20:46 +0100

    CSS changes
    adding format to main Element

--- src/script.js
+++ src/script.js
@@ -1,3 +1,5 @@
+// day and time permanently updated
+
 function displayCurrentDate(date) {
   let day = date.getDay();
   let Days = [
@@ -21,14 +23,14 @@ function displayCurrentDate(date) {
   return `${formattedDay}, ${hour}:${minutes}`;
 }
 
-let dateElement = document.querySelector(`#current-date-time`);
+let dateElement = document.querySelector("#current-date-time");
 let newDate = new Date();
 dateElement.innerHTML = displayCurrentDate(newDate);
 
 // import API data
 
 function displaySearchResults(response) {
-  let cityElement = document.querySelector("#city-Name");
+  let cityElement = document.querySelector("#city-name");
   cityElement.innerHTML = response.data.city;
   let descriptionElement = document.querySelector("#description");
   descriptionElement.innerHTML = response.data.condition.description;
@@ -37,12 +39,13 @@ function displaySearchResults(response) {
   let humidityElement = document.querySelector("#humidity");
   humidityElement.innerHTML = response.data.temperature.humidity;
   let iconElement = document.innerHTML("#current-icon");
-  iconElement.innerHTML = <img src="${response.data.condition.icon_url}" />;
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" />`;
   let currenTempElement = document.querySelector("#current-temperature");
   currenTempElement = response.data.temperture.current;
 }
 
 let apiKey = "11edc9a3d0f3o475000at9446642fb9a";
 let unit = "metric";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query={query}&key={$apiKey}&units={$unit}`;
+let query = document.querySelector("#enter-city");
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${query}&key=${apiKey}&units=${unit}`;
 axios.get(apiUrl).then(displaySearchResults);
